f3dfb6ed8e0b2f48babcfca9fb3d9cfd62f88d87
Kyle Larose <eomereadig@gmail.com>
2017-03-25 21:47:03 -0400

    Support new credentials
    - Added credentials to API (now a third step of the workflow)
    - Refactored session creation into its own function used by login and logout

--- pycapport/pycapport.py
+++ pycapport/pycapport.py
@@ -26,7 +26,7 @@ parser.add_argument('ip', help='the ip address which has been captivated')
 parser.add_argument('--logout', action="store_true", help='True to logout of an already authenticated session')
 args = parser.parse_args()
 
-def login(ip):
+def get_session(ip):
 	api_ip="10.1.0.1"
 	api_port=":5000"
 
@@ -43,16 +43,24 @@ def login(ip):
 	session_request['id_type'] = "mac"
 
 	r=requests.post(create_href, data=json.dumps(session_request), headers = headers)
-
 	print(r.text)
+	return json.loads(r.text)
 
-	return json.loads(r.text)['id']
-
+def login(ip):
+	session_json=get_session(ip)
+	login_href=session_json["cred_api_url"]	
+	login_json={}
+	login_json["username"] = "radar"
+	login_json["password"] = "radar"
+	headers = {'Accept': 'application/json'}
+	r=requests.post(login_href, data=json.dumps(login_json), headers=headers)
+	print(r.text)
+	
 def logout(id_json):
 	r=requests.delete(id_json['href'])
 	print("Logout result:", r.text)
 
 if args.logout:
-	logout(login(args.ip))
+	logout(get_session(args.ip)['id'])
 else:
 	login(args.ip)
